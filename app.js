184f2ac8d08a6583f54780e95107b4ec395154e1
Reshmitha2304 <reshmithasamanthula@gmail.com>
2019-08-18 18:09:40 +0530

    port and start command

--- app.js
+++ app.js
@@ -479,8 +479,12 @@ app.post('/upload', type, (req, res) => {
         });
 });
 
-app.listen(3000, () => {
-    console.log("on 3000");
+// app.listen(3000, () => {
+//     console.log("on 3000");
+// });
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
 });
 
 // try {
